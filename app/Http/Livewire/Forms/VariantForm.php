0becf9a63a1035fdfb7a58d525146c7da2317787
rickt25 <derricktio36@gmail.com>
2021-09-24 01:43:56 +0700

    Product Variant Crud operation before creating product

--- app/Http/Livewire/Forms/VariantForm.php
+++ app/Http/Livewire/Forms/VariantForm.php
@@ -10,7 +10,9 @@ use phpDocumentor\Reflection\Types\Boolean;
 class VariantForm extends Component
 {
     public bool $createMode = false;
+    public int $editMode = -1;
     public string $newVariant = "";
+    public string $editVariant = "";
     public array $variants = [];
 
     protected $listeners = ['refresh' => '$refresh'];
@@ -20,25 +22,49 @@ class VariantForm extends Component
             'newVariant' => [
                 'required',
                 Rule::notIn(array_column($this->variants, 'name'))
+            ],
+            'editVariant' => [
+                'required',
+                Rule::notIn(array_column($this->variants, 'name'))
             ]
         ];
     }
 
     public function addVariant(){
-        $this->validate();
-        $this->createMode = false;
+        $this->validateOnly('newVariant');
         $variant = new ProductVariant();
         $variant->name = $this->newVariant;
         array_push($this->variants, $variant);
-        $this->reset(['newVariant']);
+
+        $this->resetModes();
+    }
+
+    public function editVariant($index){
+        $this->validateOnly('editVariant');
+        $this->variants[$index]['name'] = $this->editVariant;
+
+        $this->resetModes();
     }
 
     public function deleteVariant($index){
         array_splice($this->variants, $index, 1);
     }
 
-    public function ddbutton(){
-        dd($this->variants);
+    public function showCreateForm(){
+        $this->resetModes();
+        $this->createMode = true;
+    }
+
+    public function showEditForm($index){
+        $this->resetModes();
+        $this->editMode = $index;
+        $this->editVariant = $this->variants[$index]['name'];
+    }
+    
+    public function resetModes(){
+        $this->editMode = -1;
+        $this->createMode = false;
+        $this->reset(['newVariant', 'editVariant']);
     }
 
     public function render()
--- resources/views/livewire/forms/variant-form.blade.php
+++ resources/views/livewire/forms/variant-form.blade.php
@@ -1,7 +1,7 @@
 <div>
   <div class="d-flex justify-content-between my-2 px-1">
     <h4>Variants</h4>
-    <button class="btn btn-info btn-sm" wire:click="$set('createMode', true)" title="Add variant">
+    <button class="btn btn-info btn-sm" wire:click="showCreateForm" title="Add variant">
       <i class="fas fa-plus"></i>
     </button>
   </div>
@@ -14,13 +14,29 @@
     </thead>
     <tbody>
       @forelse($variants as $variant)
-      <tr wire:key="{{ $loop->index }}">
-        <td class="line-clamp">{{ $variant['name'] ?? "null value" }}</td>
-        <td class="wrap-column px-1">
-          <button class="btn btn-sm btn-success mr-0" title="Edit"><i class="fas fa-pencil-alt"></i></button>
-          <button button class="btn btn-sm btn-danger" title="Delete" wire:click="deleteVariant({{ $loop->index }})"><i class="fas fa-trash"></i></button></span> </li>
-        </td>
-      </tr>
+        @if($editMode == $loop->index)
+          <tr>
+            <td>
+              <input class="form-control form-control-sm" wire:model="editVariant" type="text" placeholder="Name">
+            </td>
+            <td class="wrap-column px-1">
+              <button type="submit" class="btn btn-primary btn-sm mr-0" wire:click="editVariant({{ $loop->index }})">
+                <i class="fas fa-check"></i>
+              </button>
+              <button type="button" class="btn btn-danger btn-sm" wire:click.stop="resetModes">
+                <i class="fas fa-times"></i>
+              </button>
+            </td>
+          </tr>
+        @else
+          <tr wire:key="{{ $loop->index }}">
+            <td class="line-clamp">{{ $variant['name'] ?? "null value" }}</td>
+            <td class="wrap-column px-1">
+              <button class="btn btn-sm btn-success mr-0" title="Edit" wire:click="showEditForm({{ $loop->index }})"><i class="fas fa-pencil-alt"></i></button>
+              <button button class="btn btn-sm btn-danger" title="Delete" wire:click="deleteVariant({{ $loop->index }})"><i class="fas fa-trash"></i></button></span> </li>
+            </td>
+          </tr>
+        @endif
       @empty
         @if(!$createMode)
           <tr>
@@ -28,22 +44,24 @@
           </tr>
         @endif
       @endforelse
+
       @if($createMode)
-        <tr>
-          <td>
-            <input class="form-control form-control-sm" wire:model="newVariant" type="text" placeholder="Name">
-          </td>
-          <td class="wrap-column px-1">
-            <button class="btn btn-primary btn-sm mr-0" wire:click="addVariant">
-              <i class="fas fa-check"></i>
-            </button>
-            <button class="btn btn-danger btn-sm" wire:click="$set('createMode', false)">
-              <i class="fas fa-times"></i>
-            </button>
-          </td>
-        </tr>
+        <div>
+          <tr>
+            <td>
+              <input class="form-control form-control-sm" wire:model="newVariant" type="text" placeholder="Name">
+            </td>
+              <td>
+                <button type="submit" class="btn btn-primary btn-sm mr-0" wire:click="addVariant">
+                  <i class="fas fa-check"></i>
+                </button>
+                <button type="button" class="btn btn-danger btn-sm" wire:click="resetModes">
+                  <i class="fas fa-times"></i>
+                </button>
+              </td>
+          </tr>
+        </div>
       @endif
     </tbody>
   </table>
-  <button class="btn btn-primary" wire:click="ddbutton">DD button</button>
 </div>
